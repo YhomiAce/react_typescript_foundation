2c237176363020ae82a978818a95c5e74f144320
YhomiAce <kareemyomi91@gmail.com>
2023-08-14 23:40:29 +0100

    add count feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div className="text-3xl border-purple-800">
-      <h1 className="text-red-600">Hello World</h1>
+    <div className="container mx-auto px-4">
+      <h1 className="text-red-600">Hello World {count}</h1>
+      <button
+        className="rounded text-2xl bg-slate-50 p-3"
+        onClick={() => setCount(count + 1)}
+      >
+        Click me
+      </button>
     </div>
   );
 }
